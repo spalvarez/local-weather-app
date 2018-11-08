d10b143d09278907816fc9d21660c917b95950db
Sean Alvarez <alvie0818@gmail.com>
2018-11-08 18:20:36 -0500

    Add current weather component

---
+++ src/app/current-weather/current-weather.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-current-weather',
+  template: `
+    <p>current-weather works!</p>
+  `,
+  styles: [''],
+})
+export class CurrentWeatherComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
